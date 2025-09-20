5f818ed00c3a07e1eb6e0c1ee3397fc30cd74bfb
Wooyoung Jang <wyjang.physics@gmail.com>
2025-09-20 01:50:46 -0500

    demo for progress_bar

---
+++ demo.C
@@ -0,0 +1,11 @@
+{
+    int total = 1000;
+    for (int i = 1; i <= total; ++i) {
+        // 작업 수행
+        gSystem->Sleep(10);  // 예시: 1ms 대기
+
+        // 진행률 출력
+        progress_bar(i, total);
+    }
+}
+
